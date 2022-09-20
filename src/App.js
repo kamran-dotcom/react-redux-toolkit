5ef67ffad8193167769a083b386ca523fb8b2a02
kamran-dotcom <kamichannar2@gmail.com>
2022-09-20 20:17:37 +0500

    React Redux and Redux toolkit counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import { useSelector, useDispatch } from 'react-redux';
 import './App.css';
+import { actions } from './store/index';
 
 function App() {
+
+  const counter = useSelector((state)=>state.counter);
+
+  const dispatch = useDispatch();
+
+  const incre = () =>{
+    dispatch(actions.increment());
+    // dispatch({type:"INC"})
+  }
+
+  const decre = ()=>{
+    dispatch(actions.decrement())
+  }
+
+  const addBy = ()=>{
+    dispatch(actions.addBy(10));
+    // dispatch({type: "ADD", payload:10})
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{counter}</p>
+      <button onClick={incre}>Incre</button>
+      <button onClick={decre}>Decre</button>
+      <button onClick={addBy}>ADD by 10</button>
     </div>
   );
 }
